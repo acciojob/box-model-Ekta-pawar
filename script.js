dff4db331fa32da245513e7bdb729d5c18fb63fe
Ekta-pawar <ektapawar0001@gmail.com>
2025-04-28 10:35:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+.container {
+  width: 300px;
+  margin: 50px auto; 
+  padding: 20px;
+  border: 2px solid #ccc;
+  border-radius: 10px;
+  text-align: center; 
+}
+
+.red, .green, .yellow {
+  display: inline-block;
+  width: 60px;
+  height: 60px;
+  margin: 10px;
+  border-radius: 50%;
+}
+
+.red {
+  background-color: red;
+}
+
+.green {
+  background-color: green;
+}
+
+.yellow {
+  background-color: yellow;
+}
